b463c79ed14a7f243c3d6a23b0b90893cb95e6d7
Zeitaku <nurrizalzid@gmail.com>
2023-06-18 16:32:32 +0700

    init

--- mqtt_client_demo.py
+++ mqtt_client_demo.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+import threading
+from time import sleep
 
 # define static variable
 # broker = "localhost" # for local connection
@@ -9,38 +11,58 @@ timeout = 60
 username = ''
 password = ''
 
-topic = "test/lampu"
- 
+topic = "nurrizal/182"
+topic_pub = "nurrizal/182/greetings"
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code "+str(rc)+" topic: "+ client.topic)
  
     # Subscribing in on_connect() - if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe(topic,qos=2)
- 
+    client.subscribe(client.topic,qos=2)
+
+def on_publish(client,userdata,result):
+	print("data published \n")
+        
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload.decode('utf-8')))
-    payload_decoded = msg.payload.decode('utf-8')
-    if payload_decoded == "On":
-        print("Lampu dinyalakan")
-        # Do something
-        
-    elif payload_decoded == "Off":
-        print("Lampu dimatikan")
-        # Do something else
-    else:
-        print("perintah tidak di kenali")
-          
-        
+    message = str(msg.payload.decode('utf-8'))
+    if client.topic_pub != '':
+        ret = client.publish(client.topic_pub,payload=message,qos=1)
+
 # Create an MQTT client and attach our routines to it.
-client = mqtt.Client("device0")
-client.username_pw_set(username=username,password=password)
-client.on_connect = on_connect
-client.on_message = on_message
- 
-client.connect(broker, port, timeout)
 
-client.loop_forever()
+def publish(topic, topic_pub):
+    client = mqtt.Client("nurrizal/device1")
+    client.topic = topic
+    client.topic_pub = topic_pub
+    client.username_pw_set(username=username,password=password)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_publish = on_publish
+    client.connect(broker, port, timeout)
+    client.loop_forever()
 
+def greetings(topic):
+    client = mqtt.Client("nurrizal/device2")
+    client.topic = topic
+    client.topic_pub = ''
+    client.username_pw_set(username=username,password=password)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(broker, port, timeout)
+    client.loop_forever()
+
+try:
+    t1 = threading.Thread(target=publish, args=(topic,topic_pub))
+    t2 = threading.Thread(target=greetings, args=(topic_pub,))
+    t1.daemon = True
+    t2.daemon = True
+    t1.start()
+    t2.start()
+    while True:
+        sleep(1)
+except(KeyboardInterrupt, SystemExit):
+    print('Received keyboard interrupt, quitting threads.')
--- mqtt_publish_demo.py
+++ mqtt_publish_demo.py
@@ -10,29 +10,27 @@ timeout = 60
 
 username = ''
 password = ''
-topic = "test/lampu"
+topic = "nurrizal/182"
+topic_sub = "nurrizal/182/greetings"
+nama = "Nurrizal"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
  
 def on_publish(client,userdata,result):
 	print("data published \n")
-	
 
 
-client1 = paho.Client("device1",userdata=None,protocol=paho.MQTTv5)
+client1 = paho.Client("nurrizal/device0",userdata=None,protocol=paho.MQTTv5)
 client1.username_pw_set(username=username,password=password)
 client1.on_connect = on_connect
 client1.on_publish = on_publish
 client1.connect(broker,port,timeout)
 
-count = 0
-while count < 5:
-	status = str(input("Do you want the lamp turn On or Off? "))
-	message = status
+while True:
+	message = f"Hallo selamat siang, {nama}."
 	ret = client1.publish(topic,payload=message,qos=1)
 	sleep(1)
-	count = count + 1
 
 
 
